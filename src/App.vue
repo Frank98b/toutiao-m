be2e0ee94f9a95a308103fe684baeaac58f908dd
Frank_ning <512926190@qq.com>
2022-08-23 08:41:30 +0800

    添加了vant组件库，引入全部组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <div>
+      <van-cell-group>
+        <van-cell title="单元格" value="内容" />
+        <van-cell title="单元格" value="内容" label="描述信息" />
+      </van-cell-group>
+    </div>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入vant组件库
+import Vant from 'vant'
+// 引入vant所有组件
+import 'vant/lib/index.css'
 // 加载全局样式
 import './styles/index.less'
+
+// 注册vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
